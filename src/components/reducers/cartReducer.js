abb508415efd20010a1a26416275f5630884b879
BujjiKatta <85508823+BujjiKatta@users.noreply.github.com>
2021-07-02 20:37:55 +0530

    Initial commit

---
+++ src/components/reducers/cartReducer.js
@@ -0,0 +1,117 @@
+  
+import Item1 from '../../images/item1.jpg'
+import Item2 from '../../images/item2.jpg'
+import Item3 from '../../images/item3.jpg'
+import Item4 from '../../images/item4.jpg'
+import Item5 from '../../images/item5.jpg'
+import Item6 from '../../images/item6.jpg'
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING } from '../actions/action-types/cart-actions'
+
+
+const initState = {
+    items: [
+        {id:1,title:'plimsoll shoe', desc:"A pump is a type of athletic shoe with a canvas upper and rubber sole developed as beachwear in the 1830s by the Liverpool Rubber Company.", price:314,img:Item1},
+        {id:2,title:'feetwell shoe', desc: "A pair of round toe white sneakers ,has regular styling,Velcro detail,Synthetic upper Cushioned footbed Textured and patterned outsole", price:383,img: Item2},
+        {id:3,title:'plimsoll shoe', desc:"The shoe was originally, and often still is in parts of the United Kingdom, called a sand shoe and acquired the nickname plimsoll in the 1870s. This name arose, according to Nicholette Jones's book The Plimsoll Sensation ",price:600,img: Item3},
+        {id:4,title:'bespoke shoes', desc: "Bespoke shoes are shoes made especially for a certain customer by a shoemaker", price:400,img:Item4},
+        {id:5,title:'bespoke shoes', desc: "he feet are measured and a last for each foot is created. At the fitting, the customer tries the prototype pair of shoes made in an inexpensive leather and the shoemaker checks if anything needs to be changed.", price:385,img: Item5},
+        {id:6,title:'feetwell shoe', desc:"A pair of round toe white sneakers ,has regular styling,Velcro detail,Synthetic upper Cushioned footbed Textured and patterned outsole",price:436,img: Item6}
+    ],
+    addedItems:[],
+    total: 0
+
+}
+const cartReducer= (state = initState,action)=>{
+   
+    //INSIDE HOME COMPONENT
+    if(action.type === ADD_TO_CART){
+          let addedItem = state.items.find(item=> item.id === action.id)
+          //check if the action id exists in the addedItems
+         let existed_item= state.addedItems.find(item=> action.id === item.id)
+         if(existed_item)
+         {
+            addedItem.quantity += 1 
+             return{
+                ...state,
+                 total: state.total + addedItem.price 
+                  }
+        }
+         else{
+            addedItem.quantity = 1;
+            //calculating the total
+            let newTotal = state.total + addedItem.price 
+            
+            return{
+                ...state,
+                addedItems: [...state.addedItems, addedItem],
+                total : newTotal
+            }
+            
+        }
+    }
+    if(action.type === REMOVE_ITEM){
+        let itemToRemove= state.addedItems.find(item=> action.id === item.id)
+        let new_items = state.addedItems.filter(item=> action.id !== item.id)
+        
+        //calculating the total
+        let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity )
+        console.log(itemToRemove)
+        return{
+            ...state,
+            addedItems: new_items,
+            total: newTotal
+        }
+    }
+    //INSIDE CART COMPONENT
+    if(action.type=== ADD_QUANTITY){
+        let addedItem = state.items.find(item=> item.id === action.id)
+          addedItem.quantity += 1 
+          let newTotal = state.total + addedItem.price
+          return{
+              ...state,
+              total: newTotal
+          }
+    }
+    if(action.type=== SUB_QUANTITY){  
+        let addedItem = state.items.find(item=> item.id === action.id) 
+        //if the qt == 0 then it should be removed
+        if(addedItem.quantity === 1){
+            let new_items = state.addedItems.filter(item=>item.id !== action.id)
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                addedItems: new_items,
+                total: newTotal
+            }
+        }
+        else {
+            addedItem.quantity -= 1
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                total: newTotal
+            }
+        }
+        
+    }
+
+    if(action.type=== ADD_SHIPPING){
+          return{
+              ...state,
+              total: state.total + 6
+          }
+    }
+
+    if(action.type=== 'SUB_SHIPPING'){
+        return{
+            ...state,
+            total: state.total - 6
+        }
+  }
+    
+  else{
+    return state
+    }
+    
+}
+export default cartReducer
